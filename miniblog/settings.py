9dfbe24a18b5fd57e72f2e5ba836f084582878b0
Yuvraj Parashar <yuvrajparashar710@gmail.com>
2021-05-25 21:50:45 +0530

    domain name updated

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2b11cqrd#o0xp!9y(sqc9x79f7*x@e6+2o5(v(gj*!n(1b)cb)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','']
+ALLOWED_HOSTS = ['127.0.0.1','yuvi-django-blog.herokuapp.com']
 
 # Application definition
 
